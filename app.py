be360e2256bfbe3bfc61ad070104f3392ba21ffb
ram naresh <ramn@ramnMacBook-Pro.local>
2022-07-21 13:36:51 -0700

    zappa_app

---
+++ app.py
@@ -0,0 +1,12 @@
+#app.py
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+ return '<h1>Yeah, that is Zappa! Zappa! Zap! Zap!</h1>'
+
+# We only need this for local development.
+if __name__ == '__main__':
+ app.run()
